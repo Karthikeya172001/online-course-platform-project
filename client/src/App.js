d0138a33cd8cd80e69224cef08a013a1075ab105
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-07-15 17:14:37 +0530

    fix: test working App.js to remove white screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Courses from './pages/Courses';
-import AddCourse from './pages/AddCourse';
-
 function App() {
   return (
     <Router>
       <Routes>
-        {/* ✅ Simple Home route to prevent white screen */}
-        <Route path="/" element={<h1>Welcome to Online Course Platform</h1>} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/add-course" element={<AddCourse />} />
+        {/* Just a simple working route */}
+        <Route path="/" element={<h1 style={{ textAlign: 'center', marginTop: '50px' }}>✅ It works! Online Course Platform is Live</h1>} />
       </Routes>
     </Router>
   );
