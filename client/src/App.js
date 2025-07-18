0bb3194ce2f3a3daad44fd8e93c962f816e6d208
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-07-18 19:26:49 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,14 @@
-// Force re-deploy to pick up static.json
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
-import Login from './pages/Login';
-import Courses from './pages/Courses';
-import AddCourse from './pages/AddCourse';
+import Courses from './pages/Courses'; // ✅ import Courses
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1 style={{ textAlign: 'center', marginTop: '50px' }}>Welcome to Online Course Platform</h1>} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/add-course" element={<AddCourse />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/courses" element={<Courses />} /> {/* ✅ new route */}
       </Routes>
     </Router>
   );
