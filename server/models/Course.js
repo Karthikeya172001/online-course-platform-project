dfaaa2d5bd64e9bf4edda9e22e5e22157280406e
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-08-17 07:56:40 +0530

    Update Course.js

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,18 +1,9 @@
 const mongoose = require('mongoose');
 
-const courseSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  instructor: {
-    type: String,
-    required: true
-  }
+const CourseSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  instructor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
-module.exports = mongoose.model('Course', courseSchema);
+module.exports = mongoose.model('Course', CourseSchema);
