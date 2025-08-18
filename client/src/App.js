837538f644c80816aad0b00a7b00d6c8b8f7b41d
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-08-18 06:19:26 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 // client/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Courses from './pages/Courses';
+import AddCourse from './pages/AddCourse';
+import EditCourse from './pages/EditCourse'; // ✅ new
+import Navbar from './components/Navbar';
+import PrivateRoute from './utils/PrivateRoute';
 
 function App() {
   return (
@@ -13,7 +16,30 @@ function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/courses" element={<Courses />} />
+        <Route
+          path="/courses"
+          element={
+            <PrivateRoute>
+              <Courses />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/add-course"
+          element={
+            <PrivateRoute>
+              <AddCourse />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/edit-course/:id"
+          element={
+            <PrivateRoute>
+              <EditCourse />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
