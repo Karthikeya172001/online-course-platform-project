1b089f75619313455a08dc27f70b15749556d609
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-09-16 10:20:16 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,2 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://online-course-platform-project-backend.onrender.com/api",
-});
-
-export default api;
+const API_BASE_URL = "https://online-course-platform-project-backend.onrender.com/api";
+export default API_BASE_URL;
