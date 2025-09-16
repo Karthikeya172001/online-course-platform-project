84fecf8f3caaf134c91dfa6885437243894b1006
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-09-16 10:16:11 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
+  email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["student", "instructor"], default: "student" }
+  role:     { type: String, enum: ["student", "instructor"], default: "student" }
 });
 
 export default mongoose.model("User", userSchema);
