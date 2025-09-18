ede72a8217c7ca4bf36667e80fb0491cfb36dc8a
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-09-18 11:30:21 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Courses from "./pages/Courses";
-import PrivateRoute from "./components/PrivateRoute";
+import AddCourse from "./pages/AddCourse";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Navbar />
       <Routes>
+        {/* Default route → Register page */}
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/courses" element={<PrivateRoute><Courses /></PrivateRoute>} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/add-course" element={<AddCourse />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
