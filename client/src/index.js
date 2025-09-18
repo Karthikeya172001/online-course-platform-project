23db709f291e65bd6da64bb907ad2f8b79fecd7a
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-09-18 11:07:35 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <AuthProvider>
+    <ErrorBoundary>
       <App />
-    </AuthProvider>
+    </ErrorBoundary>
   </BrowserRouter>
 );
