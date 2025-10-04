7e5fca0773965ed6350bc1b0b57514864ba28783
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-10-04 07:37:40 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Courses from "./pages/Courses";
-import AddCourse from "./pages/AddCourse";
 import PrivateRoute from "./components/PrivateRoute";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-
-        {/* Protected: any logged-in user */}
-        <Route
-          path="/courses"
-          element={
-            <PrivateRoute>
-              <Courses />
-            </PrivateRoute>
-          }
-        />
-
-        {/* Protected: instructors only */}
-        <Route
-          path="/add-course"
-          element={
-            <PrivateRoute role="instructor">
-              <AddCourse />
-            </PrivateRoute>
-          }
-        />
-
-        {/* Catch-all fallback */}
-        <Route path="*" element={<p>404 Not Found</p>} />
-      </Routes>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/courses"
+            element={
+              <PrivateRoute>
+                <Courses />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
