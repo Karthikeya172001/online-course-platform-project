9b8267a6646bd589d0d0ec8a3f718a9bd8b7b275
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-10-05 16:08:40 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,12 @@
 // client/src/services/api.js
 import axios from "axios";
+const API = process.env.REACT_APP_API_URL || "https://online-course-platform-project-backend.onrender.com";
+const api = axios.create({ baseURL: `${API}/api`, headers: { "Content-Type": "application/json" } });
 
-// Use Render backend in production, localhost in dev
-const API = process.env.REACT_APP_API_URL || "http://localhost:5000";
-
-const api = axios.create({
-  baseURL: API,
-});
+api.interceptors.request.use(cfg => {
+  const t = localStorage.getItem("token");
+  if (t) cfg.headers.Authorization = `Bearer ${t}`;
+  return cfg;
+}, e => Promise.reject(e));
 
 export default api;
-
