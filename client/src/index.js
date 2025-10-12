65efdbab0eb8287a5fc63f86afc8080deebd80d3
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-10-12 14:57:08 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import ErrorBoundary from "./components/ErrorBoundary";
 
-const container = document.getElementById("root");
-createRoot(container).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
