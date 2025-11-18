e848118f0cf25c94913ed0fbb2859529aceb789b
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-11-18 21:16:09 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,52 @@
-
-
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Courses from "./pages/Courses";
 import AddCourse from "./pages/AddCourse";
-import MyCourses from "./pages/MyCourses";   // ✅ NEW PAGE
-import Navbar from "./components/Navbar";
+import MyCourses from "./pages/MyCourses";
 import PrivateRoute from "./components/PrivateRoute";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
     <Router>
       <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-
-        {/* Protected routes */}
-        <Route
-          path="/courses"
-          element={
-            <PrivateRoute>
-              <Courses />
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/add-course"
-          element={
-            <PrivateRoute>
-              <AddCourse />
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/my-courses"
-          element={
-            <PrivateRoute>
-              <MyCourses />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/courses"
+            element={
+              <PrivateRoute>
+                <Courses />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/add-course"
+            element={
+              <PrivateRoute>
+                <AddCourse />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/my-courses"
+            element={
+              <PrivateRoute>
+                <MyCourses />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </ErrorBoundary>
     </Router>
   );
 }
 
 export default App;
+
+
