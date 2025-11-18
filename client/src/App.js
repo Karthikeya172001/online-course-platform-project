a3986518cc5f73aeb081399d4a4fb40bf3cde5b9
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-11-18 17:09:28 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
+
+
+
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Courses from "./pages/Courses";
 import AddCourse from "./pages/AddCourse";
-import MyCourses from "./pages/MyCourses";
+import MyCourses from "./pages/MyCourses";   // ✅ NEW PAGE
+import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
@@ -14,48 +18,40 @@ function App() {
     <Router>
       <Navbar />
 
-      <div style={{ padding: "20px" }}>
-        <Routes>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
 
-          {/* Public Routes */}
-          <Route path="/" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+        {/* Protected routes */}
+        <Route
+          path="/courses"
+          element={
+            <PrivateRoute>
+              <Courses />
+            </PrivateRoute>
+          }
+        />
 
-          {/* Private Routes */}
-          <Route
-            path="/courses"
-            element={
-              <PrivateRoute>
-                <Courses />
-              </PrivateRoute>
-            }
-          />
+        <Route
+          path="/add-course"
+          element={
+            <PrivateRoute>
+              <AddCourse />
+            </PrivateRoute>
+          }
+        />
 
-          <Route
-            path="/add-course"
-            element={
-              <PrivateRoute>
-                <AddCourse />
-              </PrivateRoute>
-            }
-          />
-
-          <Route
-            path="/my-courses"
-            element={
-              <PrivateRoute>
-                <MyCourses />
-              </PrivateRoute>
-            }
-          />
-
-        </Routes>
-      </div>
+        <Route
+          path="/my-courses"
+          element={
+            <PrivateRoute>
+              <MyCourses />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
-
