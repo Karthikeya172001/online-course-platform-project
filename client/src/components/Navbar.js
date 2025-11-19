2ce0d2573f1b33bc7516303e9a17e37ea3f7ddd0
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-11-19 07:42:08 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import getRole from "../utils/getRole";
@@ -63,6 +64,10 @@ const styles = {
 };
 
 export default Navbar;
+
+
+
+
 
 
 
