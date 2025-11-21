3818c36b05346f9a197debf2cc6d37b2d362d57c
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-11-21 19:23:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -25,3 +26,8 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+
+
+
+
