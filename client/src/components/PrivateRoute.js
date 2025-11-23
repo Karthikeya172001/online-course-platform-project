9e55b2742b18f47c949f22f1e719817947b2288c
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-11-23 20:04:40 +0530

    Update PrivateRoute.js

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import getRole from "../utils/getRole";
 
-export default function PrivateRoute({ children }) {
+function PrivateRoute({ children, role }) {
   const token = localStorage.getItem("token");
-  if (!token) return <Navigate to="/login" replace />;
+  const userRole = getRole();
+
+  if (!token) return <Navigate to="/login" />;
+
+  if (role && role !== userRole) return <Navigate to="/courses" />;
+
   return children;
 }
+
+export default PrivateRoute;
+
+
+
+
+
